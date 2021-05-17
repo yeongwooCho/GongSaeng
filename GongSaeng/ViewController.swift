dfc23174ac62038603b39779af94540e92cca39f
yeongwoo <jyy0223@naver.com>
2021-05-17 15:43:03 +0900

    Initial Commit

---
+++ GongSaeng/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GongSaeng
+//
+//  Created by 조영우 on 2021/05/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
