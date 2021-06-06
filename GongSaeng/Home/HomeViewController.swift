30d58552f7c5af79b52e4a1d8a48594bb39febce
yeongwoo <jyy0223@naver.com>
2021-06-07 01:19:31 +0900

    ADD HomeViewController and Home storyboard

--- GongSaeng/Home/HomeViewController.swift
+++ GongSaeng/Home/HomeViewController.swift
@@ -8,22 +8,15 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    var userIdString: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print(userIdString)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
     }
-    */
-
 }
