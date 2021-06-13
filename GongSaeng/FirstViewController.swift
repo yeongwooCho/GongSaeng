8e7dc432b2a9b8395836a8ece8cd71189bcfb8bf
yeongwoo <jyy0223@naver.com>
2021-06-13 19:09:45 +0900

    Margin processing

--- GongSaeng/FirstViewController.swift
+++ GongSaeng/FirstViewController.swift
@@ -12,8 +12,6 @@ class FirstViewController: UIViewController {
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var registerButton: UIButton!
     
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
