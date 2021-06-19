3042df33f4140ec305d54c04540d9ee0a96b3111
yeongwoo <jyy0223@naver.com>
2021-06-19 18:15:01 +0900

    ADD current time value in class Date extension

---
+++ GongSaeng/Extension + Public/Date.swift
@@ -0,0 +1,24 @@
+//
+//  Date.swift
+//  GongSaeng
+//
+//  Created by 조영우 on 2021/06/13.
+//
+
+import Foundation
+
+
+extension Date {
+    public var hour: Int {
+        return Calendar.current.component(.hour, from: self)
+    }
+    
+    public var minite: Int {
+        return Calendar.current.component(.minute, from: self)
+    }
+    
+    public var second: Int {
+        return Calendar.current.component(.second, from: self)
+    }
+}
+
