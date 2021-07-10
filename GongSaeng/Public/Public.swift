9eef0dee175ada99704a121b08a3a61493c60961
yeongwoo <jyy0223@naver.com>
2021-07-10 20:06:25 +0900

    ADD maxTime property in Public

--- GongSaeng/Public/Public.swift
+++ GongSaeng/Public/Public.swift
@@ -11,18 +11,20 @@ import UIKit
 struct Public: Codable, Equatable {
     var imgTitle: String
     var title: String
-    var isDone: Bool
+    var isDone: Bool // Using
     var finalTime: Int
     var remainingTime: Int
-    var usingUser: String
+    var usingUser: String? // 사용중인 유저가 없을 수 있음
+    var maxTime: Int // 최대 사용 가능 시간
     
-    mutating func update(imgTitle: String, title: String, isDone: Bool, finalTime: Int, remainingTime: Int, usingUser: String) {
+    mutating func update(imgTitle: String, title: String, isDone: Bool, finalTime: Int, remainingTime: Int, usingUser: String, maxTime: Int) {
         self.imgTitle = imgTitle
         self.title = title
         self.isDone = isDone // true: using
         self.finalTime = finalTime
         self.remainingTime = remainingTime
         self.usingUser = usingUser // id
+        self.maxTime = maxTime
     }
 }
 
@@ -40,10 +42,10 @@ class PublicViewModel {
     }
     
     var publics: [Public] = [
-        Public(imgTitle: "qwer", title: "세탁기1사용", isDone: true, finalTime: Date().minite, remainingTime: 10, usingUser: "1234"),
-        Public(imgTitle: "asdf", title: "세탁기2안사요7ㅇ", isDone: false, finalTime: Date().minite, remainingTime: 20, usingUser: "1234"),
-        Public(imgTitle: "qwer", title: "세탁기1사용", isDone: true, finalTime: Date().minite, remainingTime: 10, usingUser: "jyy0223"),
-        Public(imgTitle: "asdf", title: "세탁기2안사용", isDone: false, finalTime: Date().minite, remainingTime: 20, usingUser: "jyy0223")
+        Public(imgTitle: "qwer", title: "세탁기1", isDone: true, finalTime: Date().minite, remainingTime: 10, usingUser: "1234", maxTime: 100),
+        Public(imgTitle: "qwer", title: "세탁기2", isDone: false, finalTime: Date().minite, remainingTime: 20, usingUser: "1234", maxTime: 90),
+        Public(imgTitle: "qwer", title: "세탁기3", isDone: true, finalTime: Date().minite, remainingTime: 30, usingUser: "jyy0223", maxTime: 80),
+        Public(imgTitle: "qwer", title: "세탁기4", isDone: false, finalTime: Date().minite, remainingTime: 40, usingUser: "jyy0223", maxTime: 70)
     ]
     
     var numOfSection: Int {
