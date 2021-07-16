a552f2865c845b52316653d6a6fa589f35ff4515
yeongwoo <jyy0223@naver.com>
2021-07-16 10:10:43 +0900

    Fix typo

---
+++ GongSaeng/Public/ReservationViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ReservationViewController.swift
+//  GongSaeng
+//
+//  Created by 조영우 on 2021/06/16.
+//
+
+import UIKit
+
+class ReservationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
